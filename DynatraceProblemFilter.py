cc365d36b89ac89dc28bf76a63f700f6985d31b8
Jordan Cogan <jordancogan4@gmail.com>
2017-03-29 16:29:06 -0500

    WIP

--- DynatraceProblemFilter.py
+++ DynatraceProblemFilter.py
@@ -1,43 +1,11 @@
-import smtplib
-import os
+import boto3
+import re
 
-def send_email(user, pwd, recipient, subject, body):
-
-  gmail_user = user
-  gmail_pwd = pwd
-  FROM = user
-  TO = recipient if type(recipient) is list else [recipient]
-  SUBJECT = subject
-  TEXT = body
-
-  # Prepare actual message
-  message = """From: %s\nTO: %s\nMIME-Version:1.0\nContent-type: text/html\nSubject: %s\n\n%s
-  """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-  try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_pwd)
-    server.sendmail(FROM, TO, message)
-    server.close()
-    print ('successfully sent the mail')
-  except:
-    print ("failed to send mail")
+#set these flags according to what problems you want to see
+SHOW_RESOLVED_PROBLEMS = False
+REQUIRE_ROOT_CAUSE = True
 
 def lambda_handler(event, context):
-  
-  #set these flags according to what problems you want to see
-  showResolvedProblems = False
-  requireRootCause = True
-  emailIntegration = True
-  
-  #set the appropriate distribution list for each tag
-  emailList = {
-    'delta.com':[],
-    'SNAPP':['jordan.cogan@dynatrace.com'],
-    'SOA':[],
-    'SafeTrac':['jordan.cogan@dynatrace.com']
-  }
-  
   # event['State']: Problem state. Possible values are OPEN or RESOLVED.
   # event['ProblemID']: Display number of the reported problem.
   # event['PID']: Unique system identifier of the reported problem.
@@ -49,29 +17,32 @@ def lambda_handler(event, context):
   # event['Tags']: Comma separated list of tags that are defined for all impacted entities.
   
   #chooses which problems to ignore
-  if not showResolvedProblems and event['State'] == 'RESOLVED':
+  if not SHOW_RESOLVED_PROBLEMS and event['State'] == 'RESOLVED':
     return "Ignoring RESOLVED problem"
-  if requireRootCause and 'Root cause' not in event['ProblemDetails']:
+  if REQUIRE_ROOT_CAUSE and 'Root cause' not in event['ProblemDetails']:
     return "Ignoring problem with no root cause"
   if event['Tags'] == '':
     return "Ignoring tagless problem"
   
-  
+  #gets the list of tags
   tagList = event['Tags'].split(', ')
   
+  #set the subject and body of the update
   subject = "%s Problem %s: %s" % (event['State'], event['ProblemID'], event['ImpactedEntity'])
-  insertPoint = event['ProblemDetails'].find('</h3>') + 5
-  if insertPoint == -1:
-    return "Ignoring because of bad HTML"
-  body = event['ProblemDetails'][:insertPoint] + '<b><span style="\&quot;font-size:110%\&quot;">Tags: </span></b>' + ", ".join(tagList) + '<br><br>' + event['ProblemDetails'][insertPoint:]
+  message = "%s\n\n Tags: %s\n\n %s" % (subject, ", ".join(tagList), event['ProblemURL'])
   
+  #replaces any SNS invalid topic characters with an underscore
+  for tag in tagList:
+    re.sub(r'[^a-zA-Z0-9-_]', '_', tag)
   
-  #send email to 
-  if emailIntegration:
-    recipients = set()
-    for tag in tagList:
-      if tag in emailList:
-        recipients = recipients | set(emailList[tag])
-    send_email(os.environ['email'], os.environ['password'], list(recipients), subject, body)
+  #push sns update
+  sns = boto3.client('sns')
+  for tag in tagList:
+    sns.publish(
+      TopicArn = tag,
+      Subject = subject,
+      Message = message,
+    )
+  
   
   return "Success!"
